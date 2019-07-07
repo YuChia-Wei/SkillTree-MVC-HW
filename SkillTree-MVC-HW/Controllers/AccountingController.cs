1cb76d942bc173f1224713b729fec85c689e6e63
魏佑嘉 <h42279@hotmail.com>
2019-07-07 15:59:21 +0800

    調整 Route，進入點改為 Accounting 而非 Home
    增加 Accounting

---
+++ SkillTree-MVC-HW/Controllers/AccountingController.cs
@@ -0,0 +1,68 @@
+﻿using SkillTree_MVC_HW.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SkillTree_MVC_HW.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult RecordList()
+        {
+            List<PersonalAccountingViewModel> showData =
+                new List<PersonalAccountingViewModel>();
+
+            GetPersonalAccountingViewModels(showData);
+
+            return View(showData);
+        }
+
+        private List<PersonalAccountingViewModel> GetPersonalAccountingViewModels()
+        {
+            return GetPersonalAccountingViewModels(new List<PersonalAccountingViewModel>());
+        }
+
+        private List<PersonalAccountingViewModel> GetPersonalAccountingViewModels(List<PersonalAccountingViewModel> orgAccountingViewModels)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                orgAccountingViewModels.Add(GetRandomAccountingViewModel(random));
+            }
+
+            return orgAccountingViewModels;
+        }
+
+        private PersonalAccountingViewModel GetRandomAccountingViewModel()
+        {
+            return GetRandomAccountingViewModel(new Random());
+        }
+
+        private PersonalAccountingViewModel GetRandomAccountingViewModel(Random random)
+        {
+            string[] accountNameStrings = new[] { "A 帳戶", "B 帳戶", "C 帳戶" };
+
+            return new PersonalAccountingViewModel()
+            {
+                Account = accountNameStrings[random.Next(0, 3)],
+                Amount = (decimal)random.Next(0, 1000000),
+                Category = (PersonalAccountingViewModel.RecordCategory)random.Next(1, 3),
+                Date = DateTime.Now.AddDays(random.Next(0, 10))
+            };
+        }
+    }
+}
---
+++ SkillTree-MVC-HW/Models/ViewModel/PersonalAccountingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkillTree_MVC_HW.Models.ViewModel
+{
+    public class PersonalAccountingViewModel
+    {
+        public enum RecordCategory
+        {
+            Income,
+            Expenditure,
+            InternalTransfer
+        }
+
+        public RecordCategory Category { get; set; }
+
+        public string Account { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public decimal Amount { get; set; }
+    }
+}
