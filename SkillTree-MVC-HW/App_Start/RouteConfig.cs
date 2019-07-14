05edfaea7f0c263ed06d52bfff5e2bab1d46d5a5
魏佑嘉 <h42279@hotmail.com>
2019-07-14 12:37:26 +0800

    調整 Route 設定，主要 HOME 為 PersonalAccounting

--- SkillTree-MVC-HW/App_Start/RouteConfig.cs
+++ SkillTree-MVC-HW/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace SkillTree_MVC_HW
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] {"SkillTree_MVC_HW.Areas.PersonalAccounting.Controllers"}
             );
         }
     }
