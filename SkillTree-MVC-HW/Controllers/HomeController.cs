fac9a48decc8f42d7466f1b34de7ffe613e168a6
魏佑嘉 <h42279@gmail.com>
2019-07-15 23:15:53 +0800

    Main: HomeController: 調整 Index 內容，顯示總額 index: 新增金額總攬相關顯示 _Layout: 調整上方連結，增加 Area 首頁的連結 (個人記帳本)

--- SkillTree-MVC-HW/Controllers/HomeController.cs
+++ SkillTree-MVC-HW/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SkillTree_MVC_HW.Repository;
+using SkillTree_MVC_HW.Service;
 
 namespace SkillTree_MVC_HW.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AccountBookService _accountBookService;
+        private readonly SkillTreeHomeWorkUnitOfWork _unitOfWork;
+
+        public HomeController()
+        {
+            _unitOfWork = new SkillTreeHomeWorkUnitOfWork();
+            _accountBookService = new AccountBookService(_unitOfWork);
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_accountBookService.Lookup());
         }
 
         public ActionResult About()
